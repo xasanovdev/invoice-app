0bf47a630e1b7db79428771801ff6b91da02989d
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2024-01-06 20:29:19 +0500

    added new invoices

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -15,10 +15,10 @@
         class="flex flex-col md:flex-row md:items-center md:mt-0 md:gap-[82px] items-start gap-2 mt-6"
       >
         <span class="text-light2 dark:text-light1 text-[13px]">{{
-          `Due ${invoice.dueDate}`
+          `Due ${invoice.paymentDue}`
         }}</span>
         <span class="text-light4 dark:text-white text-[15px] font-bold">{{
-          `£ ${invoice.amount.toFixed(2)}`
+          `£ ${invoice.total}`
         }}</span>
       </div>
 
@@ -39,11 +39,11 @@ const props = defineProps(['invoice']);
 
 const getStatusColorClass = () => {
   switch (props.invoice.status) {
-    case 'Paid':
+    case 'paid':
       return '#33D69F';
-    case 'Pending':
+    case 'pending':
       return '#FF8F00';
-    case 'Draft':
+    case 'draft':
       return '#373B53';
     default:
       return '';
